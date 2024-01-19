ac76533f4151698ce454f4797f8d4fb78a1a08b1
Daniel Ramos Sena <danielsena20@hotmail.com>
2024-01-18 23:49:20 -0300

    add + robustez calculadorJuros
    - tratamentos de exceções com try_catch melhorado
    - add método continuaOpcao
    - código mais conciso e robusto

--- calculadorJuros.java
+++ calculadorJuros.java
@@ -5,158 +5,114 @@ import java.math.RoundingMode;
 
 public class calculadorJuros {
 
-    static Scanner entrada = new Scanner(System.in, "latin1");
+    static Scanner entrada = new Scanner(System.in, "latin1"); //deixei estático pois há dois locais que é usado
 
     // Função juros simples
-    static double jSimples() {
+    static double calculaJuros(int operacao) {
 
         double valorFinal = 0.0, capital, taxaJuros;
         int tempo;
 
-        try {
-            do {
-                System.out.print("Valor inicial, em reais: ");
-                capital = entrada.nextDouble();
-            } while (capital < 0);
-            do {
-                System.out.print("Taxa de juros por mês, em porcentagem: ");
-                taxaJuros = entrada.nextDouble();
-            } while (capital < 0);
-
-            do {
-                System.out.print("Tempo, em meses: ");
-                tempo = entrada.nextInt();
-            } while (capital < 0);
-
-            valorFinal = capital + capital * (taxaJuros / 100) * tempo;
-            BigDecimal resJuros = BigDecimal.valueOf(valorFinal).setScale(2, RoundingMode.HALF_UP);
-            valorFinal = resJuros.doubleValue();
-        } catch (Exception e) {
-            System.out.println("\nErro! Apenas números são aceitos\n");
+        while (true) { //captura apenas números válidos para o valor inicial
+           try {
+             do {
+                 System.out.print("\nDigite o valor inicial: ");
+                 capital = entrada.nextDouble();
+             } while (capital < 0);
+             break;
+           } catch (InputMismatchException e) { entrada.nextLine();}
         }
+
+        while (true) { //captura apenas números válidos para a porcentagem de juros
+            try {
+                do {
+                    System.out.print("\nDigite a taxa de juros por mês, em porcentagem: ");
+                    taxaJuros = entrada.nextDouble();
+                } while (taxaJuros < 0);
+              break;
+            } catch (InputMismatchException e) { entrada.nextLine();}
+        }
+
+        while (true) { //captura apenas números válidos para o tempo de vigor do juros (em meses)
+            try {
+                do {
+                    System.out.print("\nDigite o tempo, em meses: ");
+                    tempo = entrada.nextInt();
+                } while (tempo < 0);
+              break;
+            } catch (InputMismatchException e) { entrada.nextLine();}
+        }
+
+        if(operacao == 1) valorFinal = capital + capital * (taxaJuros / 100) * tempo;   //se for juros simples faz essa conta
+        else valorFinal = capital * Math.pow((1 + (taxaJuros / 100)), tempo);           //faz caso seja juros compostos
+
+        BigDecimal resJuros = BigDecimal.valueOf(valorFinal).setScale(2, RoundingMode.HALF_UP); //arredonda o valor pra duas casas
+        valorFinal = resJuros.doubleValue(); //atribui o valor arredondado para o valor final
 
         return valorFinal;
     }
 
-    // Função juros compostos
-    static double jCompostos() {
+    static char continuaOpcao(){
+        
+        String sRepetir;
+        char repetir;
 
-        double montante = 0, capital, taxaJuros;
-        int tempo;
-
-        try {
-            do {
-                System.out.print("Digite o valor inicial: ");
-                capital = entrada.nextDouble();
-            } while (capital < 0);
-
-            do {
-                System.out.print("Digite a taxa de juros por mês, em porcentagem: ");
-                taxaJuros = entrada.nextDouble();
-            } while (taxaJuros < 0);
-
-            do {
-                System.out.print("Digite o tempo, em meses: ");
-                tempo = entrada.nextInt();
-            } while (tempo < 0);
-
-            montante = capital * Math.pow((1 + (taxaJuros / 100)), tempo);
-
-            BigDecimal resMontante = BigDecimal.valueOf(montante).setScale(2, RoundingMode.HALF_UP);
-            montante = resMontante.doubleValue();
-
-        } catch (Exception e) {
-            System.out.println("\nErro! Apenas números são aceitos\n");
-        }
-        return montante;
+        do {
+            System.out.print("Deseja fazer novamente? (s/n): ");
+            entrada.nextLine();
+            sRepetir = entrada.nextLine();
+            repetir = sRepetir.charAt(0);
+        } while (repetir != 's' && repetir != 'n');
+        System.out.println();
+        
+        return repetir;
     }
 
     // Calculadora de Juros
     public static void main(String[] args) {
 
-        String sRepetir;
         char menu = 's';
-        boolean erro = false;
 
         // Programa
-        while (menu == 's') {
+        while (menu == 's') { //fará enquanto o usuário quiser usa a calculadora
 
             int opcao = 0;
             char repetir = 's';
-
-            // Home
-            System.out.println(
-                    "\n\n\t\t--- Calculador de juros ---\n\nDigite o tipo de juros que quer calcular\n\n1. Juros simples\n2. Juros compostos\n");
+            double valorFinal = 0;
+            
+            System.out.println("\n\n\t\t--- Calculador de juros ---\n\nDigite o tipo de juros que quer calcular\n\n1. Juros simples\n2. Juros compostos\n"); // Home
 
             // Escolha da operação
-            try {
-                do {
-                    System.out.print("\tSua escolha: ");
-                    opcao = entrada.nextInt();
-                } while (opcao != 1 && opcao != 2);
-            } catch (InputMismatchException e) {
-                System.out.println("\nErro! Apenas números inteiros são aceitos\n\n");
-                erro = true;
+            while (true) { //fará enquanto não escolher 1 ou 2
+                try {
+                    do {
+                        System.out.print("\tSua escolha: ");
+                        opcao = entrada.nextInt();
+                    } while (opcao != 1 && opcao != 2);
+                    break; //se tudo der certo, sai do while(true)
+                } catch (InputMismatchException e) { entrada.nextLine(); }
             }
 
-            // Opções
+            // Lógica de juros
+            while (repetir == 's') { //fará enquanto o usuário quiser continuar a operação
 
-            if (erro == false) {
-                // Juros Simples
-                if (opcao == 1) {
-                    System.out.println("\n\n\t\t--- Juros Simples ---\n");
-                    while (repetir == 's') {
+                valorFinal = calculaJuros(opcao); //manda a opção para o método juros retornar o valor final correspondente
+                    
+                System.out.println("\n\tValor final: R$ " + valorFinal + "\n"); //printa o montante
 
-                        double valorFinal = jSimples();
-                        if (valorFinal != 0)
-                            System.out.println("\n\tValor final: R$ " + valorFinal + "\n");
+                repetir = continuaOpcao(); //verifica se o usuário quer repetir a operação
+            }
 
-                        // Repetir a conta dos Juros Simples
-                        do {
-                            System.out.print("Deseja fazer novamente? (s/n): ");
-                            entrada.nextLine();
-                            sRepetir = entrada.nextLine();
-                            repetir = sRepetir.charAt(0);
-                        } while (repetir != 's' && repetir != 'n');
-                        System.out.println("\n\t\t--------------------\n");
-                    }
-
-                }
-
-                // Juros compostos
-                else if (opcao == 2) {
-                    System.out.println("\n\n\t\t--- Juros Compostos ---\n");
-                    while (repetir == 's') {
-
-                        double valorFinal = jCompostos();
-                        if (valorFinal != 0)
-                            System.out.println("\n\tValor final: R$ " + valorFinal + "\n");
-
-                        // Repetir a conta dos Juros Compostos
-                        do {
-                            System.out.print("Deseja fazer novamente? (s/n): ");
-                            entrada.nextLine();
-                            sRepetir = entrada.nextLine();
-                            repetir = sRepetir.charAt(0);
-                        } while (repetir != 's' && repetir != 'n');
-                        System.out.println("\n\t\t-----------------------\n");
-                    }
-                }
-
-                // Voltar para o menu Encerrar o programa
-                System.out.print("\nVocê deseja voltar ao menu? (s/n) ");
-                String sMenu = entrada.nextLine();
+            // Voltar para o menu ou encerrar o programa
+            do {
+                System.out.print("Deseja voltar ao menu? (s/n) ");
+                String sMenu = entrada.nextLine(); 
                 menu = sMenu.charAt(0);
+            } while (menu != 's' && menu != 'n');
 
-            
-
-            // Fim da calculadora
-            System.out.println("\n\n\tMuito obrigado por usar essa ferramenta, aceito sugestões :)\n");
-            }
-
-            else
-                break;
         }
+
+        System.out.println("\n\n\tMuito obrigado por usar essa ferramenta, aceito sugestões :)\n"); // Mensagem de fim da calculadora
 
         entrada.close();
     }
